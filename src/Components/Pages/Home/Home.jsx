293308892db8026b003010c01aedb2832e235156
ishan <ishan.khosla22@gmail.com>
2022-09-12 20:29:58 -0400

    add hp buttons nd hammer icon

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,3 +1,5 @@
+import { Button } from "@mui/material";
+
 import React, { Component } from "react";
 import "./Home.css";
 
@@ -5,7 +7,28 @@ export default class Home extends Component {
   render() {
     return (
       <div className="container">
-        <div className="title">The Handy Man</div>
+        <div className="dataContainer">
+          <div className="titleContainer">
+            <div className="title">The Handy Man</div>
+
+            <img
+              className="hammerIcon"
+              src={require("../../../Pics/hammer.png")}
+            />
+          </div>
+          <div className="buttonContainer">
+            <div className="buttonLook">
+              <Button size="large" variant="contained">
+                Looking for a work
+              </Button>
+            </div>
+            <div className="buttonPost">
+              <Button color="secondary" size="large" variant="contained">
+                Post work
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
